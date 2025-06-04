a0252616af68cd3cc65ac88fea6f4191df456083
Dong Le <lenamsoft@gmail.com>
2025-06-04 15:36:08 +0700

    Update export_node.php
    - option to choose node type for export data

--- D7/export_node.php
+++ D7/export_node.php
@@ -4,29 +4,67 @@ include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 global $base_url;
 
-if(empty($_GET['type'])) die();
+$type = (!empty($_GET['type']))?$_GET['type']:'';
 
-$type = $_GET['type'];
+if($type) {
+    $sql = "SELECT node.nid AS nid FROM {node} node WHERE node.type = '$type' ORDER BY nid ASC ";
+     $query = db_query($sql);
 
-if($type == 'all') {
-  $query = db_query("SELECT node.nid AS nid FROM  {node} node  ORDER BY nid ASC ");
+
+    $records = $query->fetchAll();
+
+    if (empty($records)) {
+      print "";die();
+    }
+
+    
+
+    foreach ($records as $val) {
+      $nid = $val->nid;
+      $node = node_load($val->nid);
+      $url = url('node/' . $node->nid);
+
+      $node->url = $url;
+      
+
+      $data[] = $node;
+    }
+
+
+
+    header('Content-Type: application/json; charset=utf-8');
+    print json_encode($data);
+    exit();
+
 }
-
 else {
-  $query = db_query("SELECT node.nid AS nid FROM  {node} node  WHERE  (node.type =  '".$type."')  ORDER BY nid ASC");
+    display_form();
 }
 
-$records = $query->fetchAll();
-if (empty($records)) {
-  print "";die();
+function display_form() {
+
+        $all_node_types = get_all_node_types();
+        //print_r($all_node_types);
+
+        $output .= '<form method="GET">';
+
+          foreach ($all_node_types as $type) {
+            $output .= '<label>';
+            $output .= '<input type="radio" name="type" value="' . check_plain($type->type) . '">';
+            $output .= check_plain($type->type) . " - " .check_plain($type->name);
+            $output .= '</label><br>';
+          }
+
+          $output .= '<input type="submit" value="Export">';
+          $output .= '</form>';
+
+        echo $output;
 }
-$data = [];
-foreach ($records as $val) {
-  $node = node_load($val->nid);
-  //$url = $base_url . '' . url('node/' . $node->nid);
-  $data[] = $node;
+
+function get_all_node_types() {
+  $types = node_type_get_types();
+  
+  return $types;
 }
-header('Content-Type: application/json; charset=utf-8');
-print json_encode($data);
-exit();
+
 
